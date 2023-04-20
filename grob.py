74864ab175884f43fef87802e5c853f9c2a80c36
Alexander Veselov <vesselunder@gmail.com>
2023-04-20 06:00:02 +0300

    Third commit

---
+++ grob.py
@@ -0,0 +1,13 @@
+"here the docstring"
+
+
+def klopp() -> object:
+    """
+
+    :rtype: object
+    """
+    x = 1 + 1
+    return X
+
+
+klopp()
--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-#TODO find shine solution
-#FIXME lol
+"Module docstring"
 def print_hi(name) -> object:
     """
-
+TRY
     :rtype: object
     :type name: object
     """
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-x: int=10
 
-def get_fat():
-    pass
-
-frog = "green"
+def get_fat() -> object:
+    """
+string 1
+    """
+    return 1
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    get_fat()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
